9f384e34cec9c034779711423f7e5ff8b65e36f7
cllzkynk <cllzkynk@gmail.com>
2023-08-05 10:42:09 +0300

    first commit

---
+++ src/day01/FirstLesson.java
@@ -0,0 +1,33 @@
+package day01;
+
+
+
+public class FirstLesson {
+	
+ 
+	
+public static void main(String[] args) {
+	System.out.println      ("      Hello World   and My Turkish Friend   "   ); // line=satir   // ekrana yazi yazdirma komutu
+ 
+	// main metod
+	// aksi soylenene kadar tum kodlar bu mein metod icinde olacak.
+	
+	
+	
+	// JDK    // Eclipse ==> IDE 
+	
+	
+	
+	
+ 
+}
+	
+
+}
+
+
+
+
+
+
+
---
+++ src/day02/Variable.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
