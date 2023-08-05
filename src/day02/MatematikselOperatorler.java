65717a80348329af2d261f2c1b3f550ac0a1b367
cllzkynk <cllzkynk@gmail.com>
2023-08-05 11:38:06 +0300

    05/08/2023

---
+++ src/day02/MatematikselOperatorler.java
@@ -0,0 +1,30 @@
+package day02;
+
+public class MatematikselOperatorler {
+
+	public static void main(String[] args) {
+		
+		// mantik ====> VE   VEYA 
+		// ve &&
+		//veya ||
+		
+		
+		
+		System.out.println(      ""+   (  5+8   )    );
+		
+		int yas=25;
+		
+		if(yas >18) {
+			
+			System.out.println("Ehliyet alabilirsin"); 
+			
+		}else {
+			System.out.println("Ehliyet yok");
+		}
+		
+		
+		
+
+	}
+
+}
--- src/day02/Variable.java
+++ src/day02/Variable.java
@@ -3,8 +3,58 @@ package day02;
 public class Variable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		// tam sayi veri tipleri
+		 byte age_enes5      =          15  ;    //  -128 ile +127 
+	
+		 short population_elmacik = 2476 ;
+		 
+		 int    money_of_enes  =15000000;                         //integer tam sayi    **********************
+		 
+		 int    armut =15000000;      
+		 
+		 long population_world=8000000000l;
+		 
+		 
+		 //kusuratli sayisal veri tipleri 
+		 
+		 float price_house=7486378.5f;
+		 
+		 
+		 double x=94181814894846.5;
+		 
+		
+		 char harf='a';
+		 char deneme=' ';
+		 
+		 String yazi="Alcaklara kar yagmis usudun mu leylim ammman aman";
+		 
+		 String str="price_house";
+		 
+		 
+		 System.out.println(yazi);
+		
+System.out.println(price_house);
 
+
+System.out.println(str);
+
+
+
+System.out.println(5+8);
+
+System.out.println("5+8");
+
+
+
+System.out.println(      ""+5+8       );
+
+
+System.out.println(      5+8+""     );
+		
 	}
 
 }
